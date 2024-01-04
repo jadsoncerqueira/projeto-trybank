da3dec5c962afb13e75cccd742653c681f776beb
jadsoncerqueira <jadsoncerqueira97@gmail.com>
2024-01-04 05:04:29 -0300

    Implementando a função RegisterAccount() que registra novas contas

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -24,13 +24,31 @@ public class TrybankLib
     // 1. Construa a funcionalidade de cadastrar novas contas
     public void RegisterAccount(int number, int agency, int pass)
     {
-        throw new NotImplementedException();
+        for(int i = 0; i < Bank.Length; i++) {
+            if(Bank[i,0] == number && Bank[i, 1] == agency) {
+                throw new ArgumentException("A conta já está sendo usada!");
+            }
+        }
+        Bank[registeredAccounts, 0] = number;
+        Bank[registeredAccounts, 1] = agency; 
+        Bank[registeredAccounts, 2] = pass;
+        Bank[registeredAccounts, 3] = 0; 
+        registeredAccounts++;
     }
 
     // 2. Construa a funcionalidade de fazer Login
     public void Login(int number, int agency, int pass)
     {
         throw new NotImplementedException();
+        // if(Logged) {
+        //     throw new AccessViolationException();
+        // } else {
+        //     for(int i = 0; i < Bank.Length; i++) {
+        //         if(Bank[i,0] == number && Bank[i, 1] == agency) {
+        //             break
+        //         }
+        //     }
+        // }
     }
 
     // 3. Construa a funcionalidade de fazer Logout
